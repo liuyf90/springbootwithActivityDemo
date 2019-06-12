1ca0aefea09f19bf117c8ed106b5dcbb8233cc9e
liuyf <liuyf90@gmail.com>
2019-06-12 21:29:31 +0800

    init

--- src/main/java/com/example/demo2/ActivityConsumerServiceImpl.java
+++ src/main/java/com/example/demo2/ActivityConsumerServiceImpl.java
@@ -31,12 +31,20 @@ public class ActivityConsumerServiceImpl implements ActivityConsumerService {
         ExecutionEntity pi1 = (ExecutionEntity) runtimeService.startProcessInstanceByKey("leave",map);
         String processId = pi1.getId();
         String taskId = pi1.getTasks().get(0).getId();
+        System.out.println("流程实例id:"+pi1.getId());//流程实例id
+        System.out.println("流程定义id:"+pi1.getProcessDefinitionId());//输出流程定义的i
         taskService.complete(taskId, map);//完成第一步申请
 
         Task task = taskService.createTaskQuery().processInstanceId(processId).singleResult();
         String taskId2 = task.getId();
-        map.put("pass", false);
+        map.put("pass", true);
+        System.out.println("任务的办理人："+task.getAssignee());
+        System.out.println("任务的id："+task.getId());
+        System.out.println("任务的名称："+task.getName());
         taskService.complete(taskId2, map);//驳回申请
+        System.out.println("任务的办理人："+task.getAssignee());
+        System.out.println("任务的id："+task.getId());
+        System.out.println("任务的名称："+task.getName());
         System.out.println("method startActivityDemo end....");
         return false;
     }
